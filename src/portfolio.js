de93b6f14a4db81a108cb4c49eb4a249349828fa
Divyanshu <kumardivyanshu4485@gmail.com>
2021-12-31 10:02:22 +0530

    Update Page

--- src/portfolio.js
+++ src/portfolio.js
@@ -25,7 +25,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/open?id=1XYpYhLeqCdyx_q6l0bQoC7RgwQjAjXPf",
+    "https://drive.google.com/file/d/1iSgH48MVwOK89WsAMm0Ptpd9HyBjYZhH/view?usp=sharing",
   portfolio_repository:
     "https://github.com/divyanshugit/divyanshugit.github.io",
   githubProfile: "https://github.com/divyanshugit",
